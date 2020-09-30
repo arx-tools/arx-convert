b8ca447fa71b9511e3203555f983e0f25b459183
Lajos.Meszaros <m_lajos@hotmail.com>
2020-09-30 21:10:51 +0200

    Tweaked variable names

--- src/fts/SceneHeader.mjs
+++ src/fts/SceneHeader.mjs
@@ -3,15 +3,15 @@ export default class SceneHeader {
 
   readFrom(binary) {
     this.version = binary.readFloat32()
-    this.sizex = binary.readInt32()
-    this.sizez = binary.readInt32()
-    this.nb_textures = binary.readInt32()
-    this.nb_polys = binary.readInt32()
-    this.nb_anchors = binary.readInt32()
-    this.playerpos = binary.readVector3()
-    this.Mscenepos = binary.readVector3()
-    this.nb_portals = binary.readInt32()
-    this.nb_rooms = binary.readInt32()
+    this.sizeX = binary.readInt32()
+    this.sizeZ = binary.readInt32()
+    this.numberOfTextures = binary.readInt32()
+    this.numberOfPolygons = binary.readInt32()
+    this.numberOfAnchors = binary.readInt32()
+    this.playerPosition = binary.readVector3()
+    this.mScenePosition = binary.readVector3()
+    this.numberOfPortals = binary.readInt32()
+    this.numberOfRooms = binary.readInt32()
   }
 
   writeTo(binary) {}
