4c6ac6371e93cc3b15f8b597fc9550f61f8e09cb
Lajos.Meszaros <m_lajos@hotmail.com>
2020-10-11 22:23:54 +0200

    Removed never changing LightingHeader data

--- src/common/LightingHeader.mjs
+++ src/common/LightingHeader.mjs
@@ -1,14 +1,13 @@
 import BinaryIO from '../Binary/BinaryIO.mjs'
-import { repeat } from '../../node_modules/ramda/src/index.mjs'
 
 export default class LightingHeader {
   static readFrom(binary) {
     const data = {
-      numberOfLights: binary.readInt32(),
-      viewMode: binary.readInt32(), // unused
-      modeLight: binary.readInt32() // unused
+      numberOfLights: binary.readInt32()
     }
 
+    binary.readInt32() // viewMode (unused)
+    binary.readInt32() // modeLight (unused)
     binary.readInt32() // lpad
 
     return data
@@ -19,9 +18,9 @@ export default class LightingHeader {
     const binary = new BinaryIO(buffer.buffer)
 
     binary.writeInt32(json.lighting.colors.length)
-    binary.writeInt32(json.lighting.header.viewMode)
-    binary.writeInt32(json.lighting.header.modeLight)
 
+    binary.writeInt32(0)
+    binary.writeInt32(63)
     binary.writeInt32(0)
 
     return buffer
