dc4dcab0cc88bdd2942a80f9485c6befda1c756c
Lajos.Meszaros <m_lajos@hotmail.com>
2020-10-12 17:28:59 +0200

    Made color.a byte again instead of float[0..1]

--- src/common/Color.mjs
+++ src/common/Color.mjs
@@ -1,14 +1,6 @@
 import BinaryIO from '../binary/BinaryIO.mjs'
 import { getLowestByte } from "./helpers.mjs"
 
-const convertToFloatBetween0and1 = n => {
-  return (1 / 255) * n
-}
-
-const convertToByteBetween0and255 = f => {
-  return Math.round(f * 255)
-}
-
 export default class Color {
   static readFrom(binary, isCompact) {
     const data = {}
@@ -17,7 +9,7 @@ export default class Color {
       data.b = binary.readUint8()
       data.g = binary.readUint8()
       data.r = binary.readUint8()
-      data.a = convertToFloatBetween0and1(binary.readUint8())
+      data.a = binary.readUint8()
     } else {
       data.r = getLowestByte(binary.readInt32())
       data.g = getLowestByte(binary.readInt32())
@@ -36,7 +28,7 @@ export default class Color {
       binary.writeUint8(color.b)
       binary.writeUint8(color.g)
       binary.writeUint8(color.r)
-      binary.writeUint8(convertToByteBetween0and255(color.a))
+      binary.writeUint8(color.a)
     } else {
       binary.writeInt32(color.r)
       binary.writeInt32(color.g)
