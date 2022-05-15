7f6f8d5f2dc1e9d289c2a299aba6d1926237baa2
Lajos Meszaros <m_lajos@hotmail.com>
2022-05-15 13:55:51 +0200

    previous commit didn't add the new Header files, so I'm adding them now

---
+++ src/ftl/FtlHeader.js
@@ -0,0 +1,28 @@
+const BinaryIO = require("../binary/BinaryIO.js");
+const { Buffer } = require("buffer");
+
+class FtlHeader {
+  static readFrom(binary) {
+    const data = {
+      ident: binary.readString(4),
+      version: binary.readFloat32(),
+    };
+
+    return data;
+  }
+
+  static accumulateFrom(json, uncompressedSize) {
+    const buffer = Buffer.alloc(FtlHeader.sizeOf(), 0);
+    const binary = new BinaryIO(buffer.buffer);
+
+    // TODO
+
+    return buffer;
+  }
+
+  static sizeOf() {
+    return 0;
+  }
+}
+
+module.exports = FtlHeader;
---
+++ src/fts/FtsHeader.js
@@ -0,0 +1,37 @@
+const BinaryIO = require("../binary/BinaryIO.js");
+const { Buffer } = require("buffer");
+
+class FtsHeader {
+  static readFrom(binary) {
+    const data = {
+      path: binary.readString(256),
+      numberOfUniqueHeaders: binary.readInt32(),
+      version: binary.readFloat32(),
+    };
+
+    binary.readInt32(); // uncompressed size in bytes
+    binary.readUint32Array(3); // pad
+
+    return data;
+  }
+
+  static accumulateFrom(json, uncompressedSize) {
+    const buffer = Buffer.alloc(FtsHeader.sizeOf(), 0);
+    const binary = new BinaryIO(buffer.buffer);
+
+    binary.writeString(json.header.path, 256);
+    binary.writeInt32(json.uniqueHeaders.length);
+    binary.writeFloat32(json.header.version);
+    binary.writeInt32(uncompressedSize);
+
+    binary.writeUint32Array(Array(3).fill(0));
+
+    return buffer;
+  }
+
+  static sizeOf() {
+    return 280;
+  }
+}
+
+module.exports = FtsHeader;
