e5b08b705b100fc62907b898373d5141b7314e90
Lajos Meszaros <m_lajos@hotmail.com>
2022-12-27 22:55:24 +0100

    hardcode portal related properties where possible

--- src/fts/PortalPolygon.ts
+++ src/fts/PortalPolygon.ts
@@ -2,11 +2,12 @@ import { Buffer } from 'node:buffer'
 import { BinaryIO } from '../common/BinaryIO'
 import { times } from '../common/helpers'
 import { ArxVector3, QuadrupleOf } from '../common/types'
+import { ArxPolygonFlags } from '../types'
 import { ArxTextureVertex, TextureVertex } from './TextureVertex'
 
 // found in levels below 10
 // prettier-ignore
-const UNUSED_DATA_TYPE1 = [
+const HARDCODED_DATA_TYPE1 = [
    56, 242,  22,   1,    40,  22, 105,   0,   231, 255, 255, 255,     0,   0,   0,   0,
   159, 199,  92,   0,   172, 141, 105,   0,    25,   0,   0,   0,   100, 242,  22,   1,
    56, 242,  22,   1,     0,   0,   0,   0,   255, 255, 255, 255,   184, 255, 165,   0,
@@ -18,17 +19,16 @@ const UNUSED_DATA_TYPE1 = [
 ]
 
 // found in levels at or above 10
-const UNUSED_DATA_TYPE2 = UNUSED_DATA_TYPE1.slice(0)
-UNUSED_DATA_TYPE2[0x4c] -= 1
-UNUSED_DATA_TYPE2[0x5c] += 1
-UNUSED_DATA_TYPE2[0x68] += 1
-UNUSED_DATA_TYPE2[0x6c] += 1
+const HARDCODED_DATA_TYPE2 = HARDCODED_DATA_TYPE1.slice(0)
+HARDCODED_DATA_TYPE2[0x4c] -= 1
+HARDCODED_DATA_TYPE2[0x5c] += 1
+HARDCODED_DATA_TYPE2[0x68] += 1
+HARDCODED_DATA_TYPE2[0x6c] += 1
 
 /**
  * @see https://github.com/arx/ArxLibertatis/blob/1.2.1/src/graphics/data/FastSceneFormat.h#L136
  */
 export type ArxPortalPolygon = {
-  type: number
   min: ArxVector3
   max: ArxVector3
   norm: ArxVector3
@@ -40,16 +40,13 @@ export type ArxPortalPolygon = {
    */
   textureContainerId: number
   center: ArxVector3
-  transval: number
-  area: number
-  room: number
-  misc: number
 }
 
 export class PortalPolygon {
   static readFrom(binary: BinaryIO): ArxPortalPolygon {
+    binary.readInt32() // type - in 2 occasions on level 2 it's 0, all other cases it's 64 (ArxPolygonFlags.Quad)
+
     const dataBlock1 = {
-      type: binary.readInt32(),
       min: binary.readVector3(),
       max: binary.readVector3(),
       norm: binary.readVector3(),
@@ -63,11 +60,12 @@ export class PortalPolygon {
       normals: binary.readVector3Array(4) as QuadrupleOf<ArxVector3>,
       textureContainerId: binary.readInt32(),
       center: binary.readVector3(),
-      transval: binary.readFloat32(),
-      area: binary.readFloat32(),
-      room: binary.readInt16(),
-      misc: binary.readInt16(),
     }
+
+    binary.readFloat32() // transval - always 0
+    binary.readFloat32() // area - always 0
+    binary.readInt16() // room - always 0
+    binary.readInt16() // misc - always 0
 
     return {
       ...dataBlock1,
@@ -79,22 +77,28 @@ export class PortalPolygon {
     const buffer = Buffer.alloc(PortalPolygon.sizeOf())
     const binary = new BinaryIO(buffer.buffer)
 
-    binary.writeInt32(portalPolygon.type)
+    binary.writeInt32(ArxPolygonFlags.Quad)
     binary.writeVector3(portalPolygon.min)
     binary.writeVector3(portalPolygon.max)
     binary.writeVector3(portalPolygon.norm)
     binary.writeVector3(portalPolygon.norm2)
-    binary.writeBuffer(Buffer.concat(portalPolygon.vertices.map(TextureVertex.accumulateFrom)))
+    binary.writeBuffer(
+      Buffer.concat(
+        portalPolygon.vertices.map((vertex, idx) => {
+          return TextureVertex.accumulateFrom(vertex, idx)
+        }),
+      ),
+    )
 
-    binary.writeUint8Array(levelIdx < 10 ? UNUSED_DATA_TYPE1 : UNUSED_DATA_TYPE2)
+    binary.writeUint8Array(levelIdx < 10 ? HARDCODED_DATA_TYPE1 : HARDCODED_DATA_TYPE2)
 
     binary.writeVector3Array(portalPolygon.normals)
     binary.writeInt32(portalPolygon.textureContainerId)
     binary.writeVector3(portalPolygon.center)
-    binary.writeFloat32(portalPolygon.transval)
-    binary.writeFloat32(portalPolygon.area)
-    binary.writeInt16(portalPolygon.room)
-    binary.writeInt16(portalPolygon.misc)
+    binary.writeFloat32(0) // transval
+    binary.writeFloat32(0) // area
+    binary.writeInt16(0) // room
+    binary.writeInt16(0) // misc
 
     return buffer
   }
--- src/fts/TextureVertex.ts
+++ src/fts/TextureVertex.ts
@@ -3,40 +3,55 @@ import { BinaryIO } from '../common/BinaryIO'
 import { ArxColor, Color } from '../common/Color'
 import { ArxVector3 } from '../common/types'
 
+const HARDCODED_DATA_TYPE1 = {
+  color: { r: 0, g: 0, b: 0, a: 0 } as ArxColor,
+  specular: { r: 0, g: 0, b: 0, a: 0 } as ArxColor,
+  tu: 0,
+  tv: 0,
+}
+
+const HARDCODED_DATA_TYPE2 = {
+  color: { r: 1, g: 22, b: 242, a: 0.2980392156862745 } as ArxColor,
+  specular: { r: 0, g: 92, b: 200, a: 0.49411764705882355 } as ArxColor,
+  tu: 1.5694542800437951e-43,
+  tv: 2.772455559201393e-38,
+}
+
 /**
  * @see https://github.com/arx/ArxLibertatis/blob/1.2.1/src/graphics/GraphicsFormat.h#L82
  */
 export type ArxTextureVertex = {
   pos: ArxVector3
   rhw: number
-  color: ArxColor
-  specular: ArxColor
-  tu: number
-  tv: number
 }
 
 export class TextureVertex {
   static readFrom(binary: BinaryIO): ArxTextureVertex {
-    return {
+    const data = {
       pos: binary.readVector3(),
-      rhw: binary.readFloat32(), // portal bounds radius - ?
-      color: Color.readFrom(binary, 'abgr'),
-      specular: Color.readFrom(binary, 'abgr'), // unused
-      tu: binary.readFloat32(),
-      tv: binary.readFloat32(),
+      rhw: binary.readFloat32(), // portal.bounds.radius
     }
+
+    Color.readFrom(binary, 'abgr') // color
+    Color.readFrom(binary, 'abgr') // specular
+    binary.readFloat32() // tu
+    binary.readFloat32() // tv
+
+    return data
   }
 
-  static accumulateFrom(vertex: ArxTextureVertex) {
+  static accumulateFrom(vertex: ArxTextureVertex, idx: number) {
     const buffer = Buffer.alloc(TextureVertex.sizeOf())
     const binary = new BinaryIO(buffer.buffer)
 
     binary.writeVector3(vertex.pos)
-    binary.writeFloat32(vertex.rhw)
-    binary.writeBuffer(Color.accumulateFrom(vertex.color, 'abgr'))
-    binary.writeBuffer(Color.accumulateFrom(vertex.specular, 'abgr'))
-    binary.writeFloat32(vertex.tu)
-    binary.writeFloat32(vertex.tv)
+    binary.writeFloat32(idx === 0 ? vertex.rhw : 0)
+    binary.writeBuffer(Color.accumulateFrom(idx < 3 ? HARDCODED_DATA_TYPE1.color : HARDCODED_DATA_TYPE2.color, 'abgr'))
+    binary.writeBuffer(
+      Color.accumulateFrom(idx < 3 ? HARDCODED_DATA_TYPE1.specular : HARDCODED_DATA_TYPE2.specular, 'abgr'),
+    )
+    binary.writeFloat32(idx < 3 ? HARDCODED_DATA_TYPE1.tu : HARDCODED_DATA_TYPE2.tu)
+    binary.writeFloat32(idx < 3 ? HARDCODED_DATA_TYPE1.tv : HARDCODED_DATA_TYPE2.tv)
 
     return buffer
   }
