1371b93e91d1da54030ba9edefd63d34daa052c5
Lajos Meszaros <m_lajos@hotmail.com>
2023-01-01 14:49:55 +0100

    move less important data lower

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,58 @@
 import { TripleOf } from './types'
 
+export const SUPPORTED_ARX_FORMATS = ['dlf', 'fts', 'llf', 'ftl', 'tea'] as const
+export const SUPPORTED_DATA_FORMATS = ['json', 'yaml', 'yml'] as const
+export const SUPPORTED_FORMATS = [...SUPPORTED_ARX_FORMATS, ...SUPPORTED_DATA_FORMATS] as const
+
+export type SupportedArxFormat = typeof SUPPORTED_ARX_FORMATS[number]
+export type SupportedDataFormat = typeof SUPPORTED_DATA_FORMATS[number]
+export type SupportedFormat = typeof SUPPORTED_FORMATS[number]
+
+export const FTL_VERSION = 0.83257
+export const FTS_VERSION = 0.14100000262260437
+export const DANAE_VERSION = 1.440000057220459
+
+export const LITTLE_ENDIAN = true
+export const BIG_ENDIAN = false
+
+export const TRUNCATE_ZERO_BYTES = true
+export const KEEP_ZERO_BYTES = false
+
+export const MAP_WIDTH_IN_CELLS = 160
+export const MAP_DEPTH_IN_CELLS = 160
+
+// source: https://cs.stanford.edu/people/miles/iso8859.html
+// source: https://mathiasbynens.be/notes/javascript-escapes
+
+// prettier-ignore
+export const CHARS = [
+  '\0', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\b', '\t', '\n', '\v', '\f', '\r', '\x0e', '\x0f',
+  '\0x10', '\0x11', '\0x12', '\0x13', '\0x14', '\0x15', '\0x16', '\0x17', '\0x18', '\0x19', '\0x1a', '\0x1b', '\0x1c', '\0x1d', '\0x1e', '\0x1f',
+  ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
+  '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
+  '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\x7f',
+  '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89', '\x8a', '\x8b', '\x8c', '\x8d', '\x8e', '\x8f',
+  '\x90', '\x91', '\x92', '\x93', '\x94', '\x95', '\x96', '\x97', '\x98', '\x99', '\x9a', '\x9b', '\x9c', '\x9d', '\x9e', '\x9f',
+  '\xa0', '¡', '¢', '£', '¤', '¥', '¦', '§', '¨', '©', 'ª', '«', '¬', '\xad', '®', '¯',
+  '°', '±', '²', '³', '´', 'µ', '¶', '·', '¸', '¹', 'º', '»', '¼', '½', '¾', '¿',
+  'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï',
+  'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', '×', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß',
+  'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï',
+  'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ'
+]
+
+export const CODES: Record<string, number> = {}
+CHARS.forEach((value, idx) => {
+  CODES[value] = idx
+})
+
+export const BYTE_OF_AN_UNKNOWN_CHAR = CODES[' ']
+
+export const CHAR_OF_AN_UNKNOWN_BYTE = ' '
+
 // for whatever reason the following coords create a polygon, which ends up in an incorrect cell
 // when I finish averaging them and rounding them down in an attempt to find which cell they should go in
 // probably some precision got lost when Theo Game Maker exported the FTS data
@@ -136,56 +189,3 @@ export const COORDS_THAT_ROUND_UP: TripleOf<number>[] = [
   [13199.9990234375, 13200, 13200],
   [13839.9990234375, 13925, 13935],
 ]
-
-export const SUPPORTED_ARX_FORMATS = ['dlf', 'fts', 'llf', 'ftl', 'tea'] as const
-export const SUPPORTED_DATA_FORMATS = ['json', 'yaml', 'yml'] as const
-export const SUPPORTED_FORMATS = [...SUPPORTED_ARX_FORMATS, ...SUPPORTED_DATA_FORMATS] as const
-
-export type SupportedArxFormat = typeof SUPPORTED_ARX_FORMATS[number]
-export type SupportedDataFormat = typeof SUPPORTED_DATA_FORMATS[number]
-export type SupportedFormat = typeof SUPPORTED_FORMATS[number]
-
-export const FTL_VERSION = 0.83257
-export const FTS_VERSION = 0.14100000262260437
-export const DANAE_VERSION = 1.440000057220459
-
-export const LITTLE_ENDIAN = true
-export const BIG_ENDIAN = false
-
-export const TRUNCATE_ZERO_BYTES = true
-export const KEEP_ZERO_BYTES = false
-
-// source: https://cs.stanford.edu/people/miles/iso8859.html
-// source: https://mathiasbynens.be/notes/javascript-escapes
-
-// prettier-ignore
-export const CHARS = [
-  '\0', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\b', '\t', '\n', '\v', '\f', '\r', '\x0e', '\x0f',
-  '\0x10', '\0x11', '\0x12', '\0x13', '\0x14', '\0x15', '\0x16', '\0x17', '\0x18', '\0x19', '\0x1a', '\0x1b', '\0x1c', '\0x1d', '\0x1e', '\0x1f',
-  ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
-  '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-  'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
-  '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\x7f',
-  '\x80', '\x81', '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89', '\x8a', '\x8b', '\x8c', '\x8d', '\x8e', '\x8f',
-  '\x90', '\x91', '\x92', '\x93', '\x94', '\x95', '\x96', '\x97', '\x98', '\x99', '\x9a', '\x9b', '\x9c', '\x9d', '\x9e', '\x9f',
-  '\xa0', '¡', '¢', '£', '¤', '¥', '¦', '§', '¨', '©', 'ª', '«', '¬', '\xad', '®', '¯',
-  '°', '±', '²', '³', '´', 'µ', '¶', '·', '¸', '¹', 'º', '»', '¼', '½', '¾', '¿',
-  'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï',
-  'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', '×', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß',
-  'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï',
-  'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ'
-]
-
-export const CODES: Record<string, number> = {}
-CHARS.forEach((value, idx) => {
-  CODES[value] = idx
-})
-
-export const BYTE_OF_AN_UNKNOWN_CHAR = CODES[' ']
-
-export const CHAR_OF_AN_UNKNOWN_BYTE = ' '
-
-export const MAP_WIDTH_IN_CELLS = 160
-export const MAP_DEPTH_IN_CELLS = 160
