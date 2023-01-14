dd74cc546610bb62533aeeaa5309b037bda61b66
Lajos Meszaros <m_lajos@hotmail.com>
2023-01-14 17:42:41 +0100

    add a recursive version of Expand utility type

--- src/common/types.ts
+++ src/common/types.ts
@@ -41,6 +41,24 @@ export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
  * Forces vscode (and hopefully other IDEs too) to show the computed properties
  * of the given type, which is helpful when chaining a lot of generics
  *
+ * Expands only 1 level! To have the type expanded all the way use {@link RecursiveExpand}
+ *
  * @see https://github.com/microsoft/vscode/issues/94679#issuecomment-755194161
+ * @see https://stackoverflow.com/a/57683652/1806628
  */
 export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never
+
+/**
+ * Forces vscode (and hopefully other IDEs too) to show the computed properties
+ * of the given type, which is helpful when chaining a lot of generics
+ *
+ * Expands definitions all the way down. To expand only 1 level use {@link Expand}
+ *
+ * @see https://github.com/microsoft/vscode/issues/94679#issuecomment-755194161
+ * @see https://stackoverflow.com/a/57683652/1806628
+ */
+export type RecursiveExpand<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: RecursiveExpand<O[K]> }
+    : never
+  : T
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export { getCellCoords } from '@fts/helpers'
 
-export type { TripleOf, QuadrupleOf, Optional, Expand } from '@common/types'
+export type { TripleOf, QuadrupleOf, Optional, Expand, RecursiveExpand } from '@common/types'
 
 export { MAP_WIDTH_IN_CELLS, MAP_DEPTH_IN_CELLS } from '@common/constants'
