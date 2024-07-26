9b127c5321df8da6f7f17397d13d2e432f0b45a1
Lajos Meszaros <m_lajos@hotmail.com>
2024-07-26 10:44:59 +0200

    refactor(helpers): replace arrow functions with normal functions and fix other eslint issues

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,6 +1,6 @@
 import { BYTE_OF_AN_UNKNOWN_CHAR, CHARS, CHAR_OF_AN_UNKNOWN_BYTE, CODES } from '@common/constants.js'
 
-export const maxAll = (arr: number[]) => {
+export function maxAll(arr: number[]): number {
   let i = arr.length
   let max = -Infinity
 
@@ -14,26 +14,31 @@ export const maxAll = (arr: number[]) => {
 /**
  * @see https://stackoverflow.com/a/14438954/1806628
  */
-export const uniq = <T>(values: T[]) => {
+export function uniq<T>(values: T[]): T[] {
   return values.filter((value, index, self) => {
     return self.indexOf(value) === index
   })
 }
 
-export const times = <T>(fn: (index: number) => T, repetitions: number): T[] => {
-  return [...Array(repetitions)].map((value, index) => fn(index))
+export function times<T>(fn: (index: number) => T, repetitions: number): T[] {
+  return Array.from({ length: repetitions }).map((value, index) => fn(index))
 }
 
-export const repeat = <T>(value: T, repetitions: number): T[] => {
-  return Array(repetitions).fill(value)
+export function repeat<T>(value: T, repetitions: number): T[] {
+  const values = []
+  for (let i = 0; i < repetitions; i++) {
+    values.push(value)
+  }
+
+  return values
 }
 
-export const decodeText = (bytes: number[]) => {
+export function decodeText(bytes: number[]): string {
   const chars = bytes.map((byte) => CHARS[byte] ?? CHAR_OF_AN_UNKNOWN_BYTE)
   return chars.join('')
 }
 
-export const encodeText = (text: string) => {
-  const chars = text.split('')
+export function encodeText(text: string): number[] {
+  const chars = [...text]
   return chars.map((char) => CODES[char] ?? BYTE_OF_AN_UNKNOWN_CHAR)
 }
