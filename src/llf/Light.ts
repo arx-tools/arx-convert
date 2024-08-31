e06d7560772f64dd7e617e42b12f8fe9843add3d
Lajos Meszaros <m_lajos@hotmail.com>
2024-08-31 21:06:47 +0200

    docs(Light): add as much information about the flags and properties as possible

--- src/llf/Light.ts
+++ src/llf/Light.ts
@@ -8,18 +8,72 @@ import { type ArxVector3 } from '@common/types.js'
  */
 export enum ArxLightFlags {
   None = 0,
+  /**
+   * Makes the light source dynamic in a way that when combined with other flags it can
+   * be manipulated real-time in-game. For example it can be made extinguishable.
+   *
+   * A non-dynamic light is one that has their effect hardcoded into the polygon's vertex
+   * data as vertex lighting.
+   */
   SemiDynamic = 1 << 0,
+  /**
+   * Makes the light source extinguishable with the douse spell (and probably to the arrows too).
+   * Also makes it ignitable with the ignite spell.
+   *
+   * Only works if `SemiDynamic` is also set.
+   *
+   * @see https://www.youtube.com/watch?v=x8CGx19No4k on how to douse a light source with an arrow
+   */
   Extinguishable = 1 << 1,
+  /**
+   * When `SemiDynamic` is set it makes the light source start turned off.
+   * If `Extinguishable` is also set then it can be ignited.
+   */
   StartExtinguished = 1 << 2,
+  /**
+   * On its own it only makes the light source emit fire cracking sounds.
+   * When `SemiDynamic` is also set it creates a flare/halo around the light source.
+   *
+   * The flare's/halo's properties can be controlled by the properties of `ArxLight.ex*` properties
+   *
+   * Fire only shows if `ArxLight.exFrequency` is > 0
+   */
   SpawnFire = 1 << 3,
+  /**
+   * Similarly to `SpawnFire` this also makes fire cracking sounds on its own.
+   * When `SemiDynamic` is set and `ArxLight.exFrequency` is > 0 then it periodically emits
+   * smoke particles.
+   */
   SpawnSmoke = 1 << 4,
+  /**
+   * ?
+   */
   Off = 1 << 5,
+  /**
+   * ?
+   */
   ColorLegacy = 1 << 6,
   // NoCasted = 1 << 7, // unused
+  /**
+   * Normally the flare will keep its size relative to the game's window.
+   * This flag allows to break from that and have the flare size be tied to the world.
+   */
   FixFlareSize = 1 << 8,
+  /**
+   * ?
+   */
   Fireplace = 1 << 9,
-  /** blocks reacting to the player casting ignite spell, but douse will still work! */
+  /**
+   * Works together with `Extinguishable`.
+   * Blocks reacting to the ignite spell cast by the player, the spellcast script command can
+   * ignite it though.
+   *
+   * This doesn't block the light source from reacting to the douse spell.
+   */
   NoIgnit = 1 << 10,
+  /**
+   * ?
+   */
   Flare = 1 << 11,
 }
 
@@ -29,14 +83,50 @@ export enum ArxLightFlags {
 export type ArxLight = {
   pos: ArxVector3
   color: ArxColor
+  /**
+   * The radius of a sphere around `ArxLight.pos` in which the light is at full intensity.
+   */
   fallStart: number
+  /**
+   * The radius of a sphere around `ArxLight.pos` where the light's intensity gradually fades to 0.
+   */
   fallEnd: number
   intensity: number
+  /**
+   * This color periodically overrides `ArxLight.color`. The frequency of the flicker cannot be changed.
+   *
+   * TODO: what is the frequency?
+   *
+   * Subtractive color mixing is used, the color will get subtracted from `ArxLight.color` resulting in
+   * a negative color.
+   */
   exFlicker: ArxColor
   exRadius: number
+  /**
+   * How frequently should the light source spawn a flame particle when `ArxLight.flags` has `ArxLightFlags.SpawnFire`
+   * set, or a smoke particle when `ArxLight.flags` has `ArxLightFlags.SpawnSmoke` set
+   *
+   * The range is between 0.0 and 1.0 where 0 completely stops fire from spawning.
+   */
   exFrequency: number
+  /**
+   * Size of the fire/smoke particles if `ArxLightFlags.SpawnFire` or `ArxLightFlags.SpawnSmoke` is set.
+   *
+   * The value is supposed to be set between 0.0 and 1.0, but it can go over 1.0 for extreme effect.
+   */
   exSize: number
+  /**
+   * How far the fire/smoke particles go during the time they are alive. A larger number will make the particles
+   * shoot up like a fountain, a smaller number will make all of them stay in one place.
+   *
+   * The value is supposed to be set between 0.0 and 1.0, but it can go over 1.0 for extreme effect.
+   */
   exSpeed: number
+  /**
+   * The radius of the flare/halo when `ArxLight.flags` has `ArxLightFlags.SpawnFire` set.
+   *
+   * Danae only allows setting it to a maximum of 200, but there isn't any limitiations in the code
+   */
   exFlareSize: number
   flags: ArxLightFlags
 }
