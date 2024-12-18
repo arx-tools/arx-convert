54174c1ea027954774d393d755f1e1725cafcd23
Lajos Meszaros <m_lajos@hotmail.com>
2024-12-18 16:46:31 +0100

    chore(package): update typescript and fix changes of DataView in BinaryIO

--- src/common/BinaryIO.ts
+++ src/common/BinaryIO.ts
@@ -2,7 +2,8 @@ import { decodeText, encodeText, repeat } from '@common/helpers.js'
 import { LITTLE_ENDIAN, TRUNCATE_ZERO_BYTES, KEEP_ZERO_BYTES, BYTE_OF_AN_UNKNOWN_CHAR } from '@common/constants.js'
 import { type ArxQuaternion, type ArxRotation, type ArxVector3 } from '@common/types.js'
 
-export class BinaryIO extends DataView {
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments -- this is mandatory since typescript 5.7.2
+export class BinaryIO extends DataView<Uint8Array['buffer']> {
   static sizeOfFloat32(): 4 {
     return 4 as const
   }
